198de00efb7aadf5007f6a14e96d7c0e43380ab3
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-10-28 10:21:06 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,12 @@
+var x=20;
+var y=30;
+var ans=x+y;
+console.log(ans);
+var time=prompt("Whats the time ");
+if(time<5 && time>12)
+{
+    console.log("Good morning ");
+}
+else{
+  console.log("Good Evening")
+}
